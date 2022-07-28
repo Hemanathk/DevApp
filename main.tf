cb94af43faf5871f57f2740bbcef2877d8c7d07a
Hemanathk <k.hemanath@yahoo.com>
2022-07-28 23:10:16 +0530

    updated

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ provider "aws" {
 
 
 resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
+   ami           = "ami-02d1e544b84bf7502"
    instance_type = "t2.micro"
  }
